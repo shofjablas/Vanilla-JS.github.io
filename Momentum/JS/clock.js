6004b343c1013a8e43217548c8f9b6c4a03f8d86
shofjablas <shluxnsal01@gmail.com>
2024-07-01 03:17:15 +0900

    JS 완료

--- Momentum/JS/clock.js
+++ Momentum/JS/clock.js
@@ -3,9 +3,9 @@ const clock = document.querySelector('h2#clock');
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes());
-  const getSeconds = String(date.getSeconds());
-  clock.innerText = `${hours}: ${minutes}: ${getSeconds}`;
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
